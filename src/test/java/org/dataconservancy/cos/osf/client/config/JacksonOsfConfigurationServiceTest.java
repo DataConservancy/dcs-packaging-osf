76ae6356474ac2ae76c1c9359f4783e79232e350
Elliot Metsger <emetsger@jhu.edu>
2016-05-10 13:05:48 -0400

    Support for encapsulating OSF client configuration.
    Developers should create a copy of 'osf-client-jacksontest.json' and place it on the classpath.
    
    The JacksonOsfConfigurationService will attempt to resolve 'osf-client.json' from the classpath and
    use it for a default configuration.
    
    Adds:
      OsfClientConfiguration - class encapulating version 2 API configuration parameters
      OsfConfigurationService - interface responsible for producing intances of OsfClientConfiguration
      JacksonOsfConfigurationService - implementation using Jackson
    
    Includes tests.

---
+++ src/test/java/org/dataconservancy/cos/osf/client/config/JacksonOsfConfigurationServiceTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2016 Johns Hopkins University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.dataconservancy.cos.osf.client.config;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Tests for the Jackson-based configuration service
+ */
+public class JacksonOsfConfigurationServiceTest {
+
+    @Test
+    public void testSimple() throws Exception {
+        JacksonOsfConfigurationService underTest = new JacksonOsfConfigurationService("osf-client-jacksontest.json");
+
+        OsfClientConfiguration config = underTest.getConfiguration();
+        assertNotNull(config);
+
+        assertEquals("192.168.99.100", config.getHost());
+        assertEquals(8000, config.getPort());
+        assertEquals("/v2/", config.getBasePath());
+        assertEquals("foo", config.getAuthHeader());
+    }
+
+}
