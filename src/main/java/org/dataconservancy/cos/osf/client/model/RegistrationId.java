36049ae76bf7aba7defd6cbe837f09a02dca78c5
Karen Hanson <karenhanson77@gmail.com>
2016-05-10 16:12:25 -0400

    Added classes that retrieve ID only for Node, Registration, User
    The models for Node, Registration, and User involve retrieving nested
    resources through related links.  These ID classes only retrieve ID and
    pagination links to be used for iterating through ID lists.

---
+++ src/main/java/org/dataconservancy/cos/osf/client/model/RegistrationId.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2016 Johns Hopkins University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dataconservancy.cos.osf.client.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.github.jasminb.jsonapi.annotations.Type;
+
+/**
+ * Model exposes Registration ID only, so that expensive full model call is not necessary
+ * @author khanson
+ */
+@Type("registrations")
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class RegistrationId extends NodeId {
+
+	//all in NodeId
+	
+}
