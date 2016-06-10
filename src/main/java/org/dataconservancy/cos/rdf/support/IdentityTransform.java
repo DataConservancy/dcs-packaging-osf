265e8c0536f90b4c9a79d010e5ee3447dcacd1a4
Elliot Metsger <emetsger@jhu.edu>
2016-06-09 23:33:23 -0400

    Make IdentityTransform final.  Updated the implementation to forward to Function.identity(), updated Javadoc.

--- src/main/java/org/dataconservancy/cos/rdf/support/IdentityTransform.java
+++ src/main/java/org/dataconservancy/cos/rdf/support/IdentityTransform.java
@@ -3,17 +3,18 @@ package org.dataconservancy.cos.rdf.support;
 import java.util.function.Function;
 
 /**
- * Function that returns the supplied argument with no additional processing.
+ * Function that returns the supplied argument with no additional processing.  This class exists because
+ * {@link Function#identity()} cannot be the target of an annotation attribute.
  *
  * @param <T> the type of the supplied and returned argument
  */
-public class IdentityTransform<T> implements Function<T, T> {
+public final class IdentityTransform<T> implements Function<T, T> {
 
     /**
      * {@inheritDoc}
      * <h3>Implementation note:</h3>
      * <p>
-     * Simply returns the supplied object.
+     * Forwards to {@link Function#identity()}, and returns.
      * </p>
      *
      * @param t {@inheritDoc}
@@ -21,7 +22,7 @@ public class IdentityTransform<T> implements Function<T, T> {
      */
     @Override
     public T apply(T t) {
-        return t;
+        return Function.<T>identity().apply(t);
     }
 
 }
