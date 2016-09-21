6b4f7e9da0f688abf166fe9f9f604218990a82dd
Elliot Metsger <emetsger@jhu.edu>
2016-09-21 10:42:13 -0400

    Formatting Comment class.

--- src/main/java/org/dataconservancy/cos/osf/client/model/Comment.java
+++ src/main/java/org/dataconservancy/cos/osf/client/model/Comment.java
@@ -26,153 +26,176 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.github.jasminb.jsonapi.annotations.Id;
 import com.github.jasminb.jsonapi.annotations.Links;
 import com.github.jasminb.jsonapi.annotations.Type;
+
 /**
  * Comment model for OSF
- * @author khanson
  *
+ * @author khanson
+ * @author esm
  */
 @Type("comments")
 public class Comment {
 
-	/**Unique OSF id for comment*/
-	@Id
-	private String id;
-	
-	/**content of the comment*/
-	private String content;
+    /**
+     * Unique OSF id for comment
+     */
+    @Id
+    private String id;
 
-	/**timestamp that the comment was created*/
-	private DateTime date_created;
-	
-	/**timestamp when the comment was last updated*/
-	private DateTime date_modified;
+    /**
+     * content of the comment
+     */
+    private String content;
 
-	/**has this comment been edited?*/
-	private Boolean isModified;
-	
-	/**is this comment deleted?*/
-	private Boolean isDeleted;
-	
-	/**has this comment been reported by the current user?*/
-	private Boolean is_abuse;
+    /**
+     * timestamp that the comment was created
+     */
+    private DateTime date_created;
 
-	/**does this comment have replies?*/
-	private Boolean has_children;
+    /**
+     * timestamp when the comment was last updated
+     */
+    private DateTime date_modified;
 
-	/**can the current user edit this comment?*/
-	private Boolean can_edit;
+    /**
+     * has this comment been edited?
+     */
+    private Boolean isModified;
 
-	/**Gets other links found in data.links:{} section of JSON**/
-	@Links
-	Map<String, ?> links;     
+    /**
+     * is this comment deleted?
+     */
+    private Boolean isDeleted;
 
-	/** pagination links for multiple records*/
+    /**
+     * has this comment been reported by the current user?
+     */
+    private Boolean is_abuse;
+
+    /**
+     * does this comment have replies?
+     */
+    private Boolean has_children;
+
+    /**
+     * can the current user edit this comment?
+     */
+    private Boolean can_edit;
+
+    /**
+     * Gets other links found in data.links:{} section of JSON
+     **/
+    @Links
+    Map<String, ?> links;
+
+    /**
+     * pagination links for multiple records
+     */
     private org.dataconservancy.cos.osf.client.model.Links pageLinks;
 
-	public String getId() {
-		return id;
-	}
+    public String getId() {
+        return id;
+    }
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	public String getContent() {
-		return content;
-	}
+    public String getContent() {
+        return content;
+    }
 
-	public void setContent(String content) {
-		this.content = content;
-	}
+    public void setContent(String content) {
+        this.content = content;
+    }
 
     public String getDate_created() {
-    	if (this.date_created!=null) {
-    		return this.date_created.toString(DATE_TIME_FORMATTER);
-    	} else {
-    		return null;
-    	}
+        if (this.date_created != null) {
+            return this.date_created.toString(DATE_TIME_FORMATTER);
+        } else {
+            return null;
+        }
     }
 
     public void setDate_created(String date_created) {
-    	if (date_created!=null){
-			this.date_created = JodaSupport.parseDateTime(date_created);
-		} else {
-    		this.date_created = null;
-    	}
+        if (date_created != null) {
+            this.date_created = JodaSupport.parseDateTime(date_created);
+        } else {
+            this.date_created = null;
+        }
     }
 
     public String getDate_modified() {
-    	if (this.date_modified!=null) {
-    		return this.date_modified.toString(DATE_TIME_FORMATTER);
-    	} else {
-    		return null;
-    	}
+        if (this.date_modified != null) {
+            return this.date_modified.toString(DATE_TIME_FORMATTER);
+        } else {
+            return null;
+        }
     }
 
     public void setDate_modified(String date_modified) {
-    	if (date_modified!=null){
-			this.date_modified = JodaSupport.parseDateTime(date_modified);
-		} else {
-    		date_modified=null;
-    	}
+        if (date_modified != null) {
+            this.date_modified = JodaSupport.parseDateTime(date_modified);
+        } else {
+            date_modified = null;
+        }
     }
 
-	public Boolean isModified() {
-		return isModified;
-	}
+    public Boolean isModified() {
+        return isModified;
+    }
 
-	public void setModified(Boolean isModified) {
-		this.isModified = isModified;
-	}
+    public void setModified(Boolean isModified) {
+        this.isModified = isModified;
+    }
 
-	public Boolean isDeleted() {
-		return isDeleted;
-	}
+    public Boolean isDeleted() {
+        return isDeleted;
+    }
 
-	public void setDeleted(Boolean isDeleted) {
-		this.isDeleted = isDeleted;
-	}
+    public void setDeleted(Boolean isDeleted) {
+        this.isDeleted = isDeleted;
+    }
 
-	public Boolean isIs_abuse() {
-		return is_abuse;
-	}
+    public Boolean isIs_abuse() {
+        return is_abuse;
+    }
 
-	public void setIs_abuse(Boolean is_abuse) {
-		this.is_abuse = is_abuse;
-	}
+    public void setIs_abuse(Boolean is_abuse) {
+        this.is_abuse = is_abuse;
+    }
 
-	public Boolean isHas_children() {
-		return has_children;
-	}
+    public Boolean isHas_children() {
+        return has_children;
+    }
 
-	public void setHas_children(Boolean has_children) {
-		this.has_children = has_children;
-	}
+    public void setHas_children(Boolean has_children) {
+        this.has_children = has_children;
+    }
 
-	public Boolean isCan_edit() {
-		return can_edit;
-	}
+    public Boolean isCan_edit() {
+        return can_edit;
+    }
 
-	public void setCan_edit(Boolean can_edit) {
-		this.can_edit = can_edit;
-	}
+    public void setCan_edit(Boolean can_edit) {
+        this.can_edit = can_edit;
+    }
 
-	public org.dataconservancy.cos.osf.client.model.Links getPageLinks() {
-		return pageLinks;
-	}
+    public org.dataconservancy.cos.osf.client.model.Links getPageLinks() {
+        return pageLinks;
+    }
 
     @JsonProperty("links")
-	public void setPageLinks(org.dataconservancy.cos.osf.client.model.Links pageLinks) {
-		this.pageLinks = pageLinks;
-	}
+    public void setPageLinks(org.dataconservancy.cos.osf.client.model.Links pageLinks) {
+        this.pageLinks = pageLinks;
+    }
 
-	public Map<String, ?> getLinks() {
-		return links;
-	}
+    public Map<String, ?> getLinks() {
+        return links;
+    }
 
-	public void setLinks(Map<String, ?> links) {
-		this.links = links;
-	}
+    public void setLinks(Map<String, ?> links) {
+        this.links = links;
+    }
 
-	
 }
