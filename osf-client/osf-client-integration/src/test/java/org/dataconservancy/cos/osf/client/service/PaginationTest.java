2558b5eaac6a89670d6973008af8915dc6562b24
Elliot Metsger <emetsger@jhu.edu>
2017-01-23 15:10:50 -0500

    Pagination support: update the javadoc and logic of the file pagination test to be a bit more specific, including testing the order of the retrieved objects.

--- osf-client/osf-client-integration/src/test/java/org/dataconservancy/cos/osf/client/service/PaginationTest.java
+++ osf-client/osf-client-integration/src/test/java/org/dataconservancy/cos/osf/client/service/PaginationTest.java
@@ -24,6 +24,7 @@ import org.dataconservancy.cos.osf.client.model.Node;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -66,14 +67,36 @@ public class PaginationTest extends AbstractMockServerTest {
 //        assertEquals(Integer.valueOf(20), resultsPage.getMeta().get("total"));
 //    }
 
+    /**
+     * Retrieves a {@code Node}, traverses the {@code files} relationship, insuring that all the files are retrieved in
+     * order, despite the fact that the {@code files} relationship is paginated.
+     * <p>
+     * Because {@code Node} defines a {@link Node#files} as a
+     * {@link com.github.jasminb.jsonapi.annotations.Relationship} with a strategy of
+     * {@link com.github.jasminb.jsonapi.ResolutionStrategy#OBJECT}, the JSONAPI converter will retrieve all the files
+     * associated with the node.
+     * </p>
+     * @throws IOException if the {@code Node} cannot be retrieved by the test {@code osfService}
+     */
     @Test
-    public void testPaginationOfFiles() throws Exception {
+    public void testPaginationOfResolvedRelationship() throws IOException {
         final Node n = osfService.nodeByUrl("http://localhost:8000/v2/nodes/xmnkz/").execute().body();
 
+        // xmnkz node has a single file provider
+        assertEquals(1, n.getFiles().size());
         final File provider = n.getFiles().get(0);
         assertEquals("xmnkz:osfstorage", provider.getId());
 
+        // the xmnkz:osfstorage provider has 11 files across two pages.  The API does not expose the notion of pages
+        // when relationships are traversed by the JSONAPI converter (the relationships are traversed by the JSONAPI
+        // converter implementation, and are not able to be influenced by the caller), so we cannot demonstrate that two
+        // pages were consulted when producing the response.  However, the raw JSON is available on the classpath at
+        // localhost/8000/v2/nodes/xmnkz/files/osfstorage/
         final List<File> files = provider.getFiles();
         assertEquals(11, files.size());
+
+        // Also serves to demonstrate that the order is preserved as encounter order in the JSON.
+        assertEquals("587fdf4bad3c4800474d7eb3", files.get(0).getId());
+        assertEquals("587fdf666c613b0043f2c804", files.get(10).getId());
     }
 }
